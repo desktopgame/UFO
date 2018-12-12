f075cfbb72f4a89a79af898a2747f2f0459a279a
Koya <koya0306@outlook.jp>
2018-12-12 13:50:29 +0900

    最初のコミット

---
+++ Assets/Script/UI/FadeUI.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// フェード表現のためのスクリプト。
+/// </summary>
+public class FadeUI : CachedMonoBehaviour<FadeUI> {
+    [SerializeField]
+    private Image image;
+
+    [SerializeField]
+    private float fadeSeconds = 1f;
+
+	// Use this for initialization
+	void Start () {
+		if(this.image == null)
+        {
+            this.image = GetComponentInChildren<Image>();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartFade(System.Action doInBackground)
+    {
+        StartCoroutine(FadeWithAction(doInBackground));
+    }
+
+    private IEnumerator FadeWithAction(System.Action doInBackground)
+    {
+        yield return FadeUpdate(fadeSeconds, (e) => e);
+        doInBackground();
+        yield return FadeUpdate(fadeSeconds, (e) => 1 - e);
+    }
+
+    private IEnumerator FadeUpdate(float seconds, System.Func<float,float> calcAlpha)
+    {
+        var offset = 0f;
+        var separate = 100f;
+        var segment = seconds / separate;
+        var baseColor = image.color;
+        while(offset < seconds)
+        {
+            yield return new WaitForSeconds(segment);
+            offset += segment;
+            var parcent = offset / seconds;
+            var alpha = calcAlpha(parcent);
+            baseColor.a = alpha;
+            image.color = baseColor;
+        }
+        baseColor.a = calcAlpha(1f);
+        image.color = baseColor;
+    }
+}
---
+++ Assets/Script/Util/CachedMonoBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// シーンをまたいで存在し続けるオブジェクトの基底クラス。
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class CachedMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour {
+    /// <summary>
+    /// 唯一のインスタンス。
+    /// </summary>
+    public static T instance { private set; get; }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = FindObjectOfType<T>();
+            DontDestroyOnLoad(gameObject);
+            PostAwake();
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    /// <summary>
+    /// インスタンス生成直後に呼ばれます。
+    /// </summary>
+    protected virtual void PostAwake()
+    {
+
+    }
+}
