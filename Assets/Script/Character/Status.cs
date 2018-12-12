21f3908a31bf825c60219d0874715110f6e8eb24
Koya <koya0306@outlook.jp>
2018-12-12 19:35:53 +0900

    Statusを追加

---
+++ Assets/Script/Character/Status.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class StatusEvent {
+	public Status source { private set; get; }
+
+	public StatusEvent(Status source) {
+		this.source = source;
+	}
+}
+
+/// <summary>
+/// ステータスを表すクラス。
+/// </summary>
+public class Status : MonoBehaviour {
+	[SerializeField]
+	private float maxHP = 100;
+
+	/// <summary>
+	/// ダメージを受けると呼ばれます。
+	/// </summary>
+	/// <value></value>
+	public IObservable<StatusEvent> onDamage { get { return damage; }}
+	private Subject<StatusEvent> damage;
+
+	public float currentHP { private set; get; }
+
+	// Use this for initialization
+	void Start () {
+		this.currentHP = maxHP;
+		this.damage = new Subject<StatusEvent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/// <summary>
+	/// HPの割合を返します。
+	/// </summary>
+	/// <returns></returns>
+	public float GetHPRatio() {
+		return Mathf.Clamp01(currentHP / maxHP);
+	}
+
+	/// <summary>
+	/// 最大HPを返します。
+	/// </summary>
+	/// <returns></returns>
+	public float GetMaxHP() {
+		return maxHP;
+	}
+
+	/// <summary>
+	/// HPを減らします。
+	/// </summary>
+	/// <param name="power"></param>
+	public void Damage(float power) {
+		this.currentHP = Mathf.Max(0f, currentHP - power);
+		damage.OnNext(new StatusEvent(this));
+	}
+}
