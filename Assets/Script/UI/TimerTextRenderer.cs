df40897876479271aa6b344236df0ac3cc2e5e76
Koya <koya0306@outlook.jp>
2018-12-12 14:33:37 +0900

    タイマー仮実装

---
+++ Assets/Script/UI/TimerTextRenderer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+/// <summary>
+/// タイマーの更新と同時にテキストを更新するレンダラー。
+/// </summary>
+public class TimerTextRenderer : MonoBehaviour {
+	[SerializeField]
+	private TimerUI timer;
+
+	[SerializeField]
+	private Text text;
+
+	private System.IDisposable onElapsedObserver;
+	private System.IDisposable onTickObserver;
+
+	// Use this for initialization
+	void Start () {
+		if(timer == null) {
+			this.timer = GetComponent<TimerUI>();
+		}
+		if(text == null) {
+			this.text = GetComponent<Text>();
+		}
+		this.onElapsedObserver = timer.onTick.Subscribe((e) => {
+			text.text = e.source.remine.ToString();
+		});
+		this.onTickObserver = timer.onElapsed.Subscribe((e) => {
+			text.text = "0";
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnDestroy() {
+		onElapsedObserver.Dispose();
+		onTickObserver.Dispose();
+	}
+}
---
+++ Assets/Script/UI/TimerUI.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+/// <summary>
+/// タイマーの更新イベント。
+/// </summary>
+public class TimerEvent {
+	public TimerUI source { private set; get; }
+
+	public TimerEvent(TimerUI source) {
+		this.source = source;
+	}
+}
+
+/// <summary>
+/// タイマー表示のためのスクリプト。
+/// </summary>
+public class TimerUI : MonoBehaviour {
+	[SerializeField]
+	private int length = 100;
+
+	[SerializeField]
+	private float timeUnit = 1f;
+
+	/// <summary>
+	/// 残り時間が減るたびに呼ばれる。
+	/// </summary>
+	/// <value></value>
+	public IObservable<TimerEvent> onTick { get { return tick; }}
+	private Subject<TimerEvent> tick;
+
+	/// <summary>
+	/// 残り時間が 0 になる瞬間を通知するイベント。
+	/// </summary>
+	/// <value></value>
+	public IObservable<TimerEvent> onElapsed { get { return elapsed; }}
+	private Subject<TimerEvent> elapsed;
+
+	/// <summary>
+	/// 残り時間。
+	/// </summary>
+	/// <value></value>
+	public int remine { private set; get;}
+
+	/// <summary>
+	/// 残り時間(0~1)。
+	/// </summary>
+	/// <returns></returns>
+	public float parcent { get { return (float)remine / (float)length; }}
+
+	// Use this for initialization
+	void Awake () {
+		this.remine = length;
+		this.tick = new Subject<TimerEvent>();
+		this.elapsed = new Subject<TimerEvent>();
+		tick.OnNext(new TimerEvent(this));
+		StartCoroutine(ProgressUpdate());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private IEnumerator ProgressUpdate() {
+		var wait = new WaitForSeconds(timeUnit);
+		while(remine > 0) {
+			yield return wait;
+			this.remine -= 1;
+			tick.OnNext(new TimerEvent(this));
+		}
+		elapsed.OnNext(new TimerEvent(this));
+	}
+}
