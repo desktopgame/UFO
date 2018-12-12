da26bf24a8dd9b9415b0c23a92d0058f75845422
Koya <koya0306@outlook.jp>
2018-12-12 19:40:18 +0900

    回転を固定するためのスクリプト追加

---
+++ Assets/Script/Util/LockRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 回転の状態を固定します。
+/// </summary>
+public class LockRotation : MonoBehaviour {
+	private Quaternion rotation;
+	// Use this for initialization
+	void Start () {
+		this.rotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = rotation;
+	}
+}
