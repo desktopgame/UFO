5dc6b2e6897c16b219c20b2a84078e32eb90eb01
Koya <koya0306@outlook.jp>
2018-12-13 11:18:56 +0900

    スコア関連追加

---
+++ Assets/Scripts/Score/ScoreManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// スコア情報の管理。
+/// </summary>
+/// <typeparam name="ScoreManager"></typeparam>
+public class ScoreManager : SingletonMonoBehaviour<ScoreManager> {
+	public int maxValue { private set; get; }
+	public int currentValue { private set; get; }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/// <summary>
+	/// スコアをリセットします。
+	/// </summary>
+	/// <param name="maxValue"></param>
+	public void Reset(int maxValue) {
+		this.maxValue = maxValue;
+		this.currentValue = 0;
+	}
+}
---
+++ Assets/Scripts/Score/ScoreReset.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ゲーム開始時にスコアをリセットする。
+/// </summary>
+public class ScoreReset : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var len = GameObject.FindGameObjectsWithTag("PickUp").Length;
+		ScoreManager.Instance.Reset(len);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
