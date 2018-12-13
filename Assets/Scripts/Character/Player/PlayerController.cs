875af567d2d4c1c4a537a895d93a2ef5b4570bb7
Koya <koya0306@outlook.jp>
2018-12-13 17:59:07 +0900

    ブレーキ実装

--- Assets/Scripts/Character/Player/PlayerController.cs
+++ Assets/Scripts/Character/Player/PlayerController.cs
@@ -7,6 +7,9 @@ using System;
 
 public class PlayerController : MonoBehaviour {
     public float speed;
+    public float brakeWait = 1f;
+
+
     private Rigidbody2D rb2d;
     private int count;
     private System.IDisposable observer;
@@ -22,6 +25,9 @@ public class PlayerController : MonoBehaviour {
     }
     private bool mFreeze;
 
+    private bool brakeTriggered;
+    private float brakeElapsed;
+
     // Use this for initialization
     void Start() {
         rb2d = GetComponent<Rigidbody2D>();
@@ -36,6 +42,20 @@ public class PlayerController : MonoBehaviour {
 
     void FixedUpdate() {
         if(freeze) {
+            return;
+        }
+        if(brakeTriggered) {
+            if((brakeElapsed += Time.deltaTime) > brakeWait) {
+                brakeTriggered = false;
+            }
+            return;
+        }
+        //ブレーキ
+        if(Input.GetKey(KeyCode.LeftShift) ||
+           Input.GetKey(KeyCode.RightShift)) {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            this.brakeElapsed = 0f;
+            this.brakeTriggered = true;
             return;
         }
         float moveHorizontal = Input.GetAxis("Horizontal");
