d53c669866f0e2ce2a6a196ab5f7f2d81fd6b121
Koya <koya0306@outlook.jp>
2018-12-13 14:30:52 +0900

    クレジット画面追加

---
+++ Assets/Scripts/UI/LinkByPush.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ボタンが押されたら指定のURLへジャンプ
+/// </summary>
+public class LinkByPush : MonoBehaviour {
+	[SerializeField]
+	private string url;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void JumpURL() {
+		Application.OpenURL(url);
+	}
+}
